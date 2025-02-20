41baf16f3939720fa8a91c27a006994bfb217cb7
UP240092 <up240092@alumnos.upa.edu.mx>
2025-02-20 11:35:21 -0600

    1-7

---
+++ Ejercicio1.py
@@ -0,0 +1,39 @@
+#Ejercicio1: Edad
+edad= 18
+
+#Ejercicio 2: Altura
+altura= 1.73
+
+#Ejercicio 3: Número complejo
+nc= 4+10j
+
+#Ejercicio 4: Area de un triangulo
+altura= float(input("Coloca la altura del triangulo: "))
+base= float(input("Coloca la base del triangulo: "))
+area= base*altura/2
+print("El area de tu triangulo es: ", area)
+
+#Ejercicio 5: Perimetro de un triangulo
+ladoa= float(input("Inserta lado A: "))
+ladob= float(input("Inserta lado B: "))
+ladoc= float(input("Inserta lado C: "))
+perimetro= ladoa+ladob+ladoc
+print("El perimetro de tu triangulo es: ", perimetro)
+
+#Ejercicio 6: Area y perimetro de un rectangulo
+alturar= float(input("Coloca la altura del rectangulo: "))
+baser= float(input("Coloca la base del rectangulo: "))
+arear= baser*alturar
+perimetror= 2*(baser+alturar)
+print("El area de tu rectangulo es: ", arear)
+print("El perimetro de tu rectangulo es: ", perimetror)
+
+#Ejercicio 7: Area y Circunferencia de un circulo
+radio= float(input("Coloca el radio del circulo: "))
+pi= 3.14
+areac= pi*radio*radio
+circunferencia= 2*pi*radio
+print("El area de tu circulo es: ", areac)
+print("La circunferencia de tu circulo es: ", circunferencia)
+
+#Ejercicio 8: 
