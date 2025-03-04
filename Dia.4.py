5fc5f42ae03f5c5759f08c992254a3277d805566
UP240092 <up240092@alumnos.upa.edu.mx>
2025-03-04 09:32:18 -0600

    dia 4

---
+++ Dia.4.py
@@ -0,0 +1,107 @@
+#Ejercicio 1:
+print("Ejercicio 1")
+
+#Ejercicio 2:
+print("Ejercicio 2")
+
+#Ejercicio 3:
+print("Ejercicio 3")
+
+#Ejercicio 4:
+print("Ejercicio 4")
+
+#Ejercicio 5:
+print("Ejercicio 5")
+
+#Ejercicio 6:
+print("Ejercicio 6")
+
+#Ejercicio 7:
+print("Ejercicio 7")
+
+#Ejercicio 8: 
+print("Ejercicio 8")
+
+#Ejercicio 9:
+print("Ejercicio 9")
+
+#Ejercicio 10:
+print("Ejercicio 10")
+
+#Ejercicio 11:
+print("Ejercicio 11")
+
+#Ejercicio 12:
+print("Ejercicio 12")
+
+#Ejercicio 13:
+print("Ejercicio 13")
+
+#Ejercicio 14:
+print("Ejercicio 14")
+
+#Ejercicio 15:
+print("Ejercicio 15")
+
+#Ejercicio 16:
+print("Ejercicio 16")
+
+#Ejercicio 17:
+print("Ejercicio 17")
+
+#Ejercicio 18:
+print("Ejercicio 18")
+
+#Ejercicio 19:
+print("Ejercicio 19")
+
+#Ejercicio 20:
+print("Ejercicio 20")
+
+#Ejercicio 21:
+print("Ejercicio 21")
+
+#Ejercicio 22:
+print("Ejercicio 22")
+
+#Ejercicio 23:
+print("Ejercicio 23")
+
+#Ejercicio 24:
+print("Ejercicio 24")
+
+#Ejercicio 25:
+print("Ejercicio 25")
+
+#Ejercicio 26:
+print("Ejercicio 26")
+
+#Ejercicio 27:
+print("Ejercicio 27")
+
+#Ejercicio 28:
+print("Ejercicio 28")
+
+#Ejercicio 29:
+print("Ejercicio 29")
+
+#Ejercicio 30:
+print("Ejercicio 30")
+
+#Ejercicio 31:
+print("Ejercicio 31")
+
+#Ejercicio 32:
+print("Ejercicio 32")
+
+#Ejercicio 33:
+print("Ejercicio 33")
+
+#Ejercicio 34:
+print("Ejercicio 34")
+
+#Ejercicio 35:
+print("Ejercicio 35")
+
+#Ejercicio 36:
+print("Ejercicio 36")
