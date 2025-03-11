9ec3621926ef06d9568148eb3e431e9ff71e3753
UP240092 <up240092@alumnos.upa.edu.mx>
2025-03-11 08:17:53 -0600

    1-17

--- Dia.5.py
+++ Dia.5.py
@@ -23,7 +23,7 @@ print("Se hiso una variable con mis datos personales")
 
 #Ejercicio 6: Hacer una variable con lista de compañias
 print("Ejercicio 6")
-it_companies= ("Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon")
+it_companies= ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
 print("Se hiso una variable con una lista de distintas compañias")
 
 #Ejercicio 7: Imprimir la lista anterior
@@ -40,28 +40,41 @@ print(it_companies[0], it_companies[3], it_companies[6])
 
 #Ejercicio 10: Modificar una empresa e imprimir la nueva lista
 print("Ejercicio 10")
-it_companies[6]= 'Samsung'
+print(it_companies)
+it_companies[3]= 'Samsung'
 print(it_companies)
 
-#Ejercicio 11: 
+#Ejercicio 11: Agregar una empresa de TI en la lista
 print("Ejercicio 11")
+it_companies.append("Dell")
+print(it_companies)
 
-#Ejercicio 12:
+#Ejercicio 12: Agregar una empresa de TI en medio de la lista
 print("Ejercicio 12")
+it_companies.insert(4, "Accenture")
+print(it_companies)
 
-#Ejercicio 13:
+#Ejercicio 13: Cambiar el nombre de una empresa a mayusculas
 print("Ejercicio 13")
+it_companies[3]= it_companies[3].upper()
+print(it_companies)
 
-#Ejercicio 14:
+#Ejercicio 14: Hacer una cadena de las empresas
 print("Ejercicio 14")
+cadena= "#; ".join(it_companies)
+print(cadena)
 
-#Ejercicio 15:
+#Ejercicio 15: Revisar si existe una empresa en la lista
 print("Ejercicio 15")
+exis= "Tesla" in it_companies
+print(exis)
 
-#Ejercicio 16:
+#Ejercicio 16: Organisar la lista en orden alfabetico
 print("Ejercicio 16")
+it_companies.sort()
+print(it_companies)
 
-#Ejercicio 17:
+#Ejercicio 17: Organizar la lista de manera alfabetica al reves
 print("Ejercicio 17")
 
 #Ejercicio 18:
