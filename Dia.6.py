45b41247a97b264885ff85dd3d4cd8861c81ad2a
UP240092 <up240092@alumnos.upa.edu.mx>
2025-03-12 10:01:58 -0600

    1-5

---
+++ Dia.6.py
@@ -0,0 +1,53 @@
+#Nivel 1, Ejercicio 1: Crear una tupla vacia
+print("Nivel 1, Ejercicio 1")
+tupla= tuple()
+print(tupla)
+
+#Nivel 1, Ejercicio 2: Tupla con los nombres de mis hermanos
+print("Nivel 1, Ejercicio 2")
+sis= ("Karina", "Julieta")
+bro= ("Andres", "Luisda")
+print(sis, bro)
+
+#Nivel 1, Ejercicio 3: Juntar las dos tuplas
+print("Nivel 1, Ejercicio 3")
+seb= sis+bro
+print(seb)
+
+#Nivel 1, Ejercicio 4: Sacar la longitud de la tupla
+print("Nivel 1, Ejercicio 4")
+print(len(seb))
+
+#Nivel 1, Ejercicio 5: Agrgar los nombres de mis papás
+print("Nivel 1, Ejercicio 5")
+par= ("Ana", "Julio")
+fam= seb+par
+print(fam)
+
+#Nivel 2, Ejercicio 1: Desempacar todos los de la tupla
+print("Nivel 2, Ejercicio 1")
+
+
+#Nivel 2, Ejercicio 2:
+print("Nivel 2, Ejercicio 2")
+
+#Nivel 2, Ejercicio 3:
+print("Nivel 2, Ejercicio 3")
+
+#Nivel 2, Ejercicio 4:
+print("Nivel 2, Ejercicio 4")
+
+#Nivel 2, Ejercicio 5:
+print("Nivel 2, Ejercicio 5")
+
+#Nivel 2, Ejercicio 6:
+print("Nivel 2, Ejercicio 6")
+
+#Nivel 2, Ejercicio 7.0:
+print("Nivel 2, Ejercicio 7.0")
+
+#Nivel 2, Ejercicio 7.1:
+print("Nivel 2, Ejercicio 7.1")
+
+#Nivel 2, Ejercicio 7.2:
+print("Nivel 2, Ejercicio 7.2")
