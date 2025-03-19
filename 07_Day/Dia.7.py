ba8461794f6ca80c275c648454ad0d4316e6d9dc
UP240092 <up240092@alumnos.upa.edu.mx>
2025-03-19 10:39:34 -0600

    n1

--- 07_Day/Dia.7.py
+++ 07_Day/Dia.7.py
@@ -1,17 +1,27 @@
-#Nivel 1, Ejercicio 1:
+#Nivel 1, Ejercicio 1: Encontrar la longitud de it-companies
 print("Nivel 1, Ejercicio 1")
+it_companies= {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+print(len(it_companies))
 
-#Nivel 1, Ejercicio 2:
+#Nivel 1, Ejercicio 2: Agregar twitter en it_companies
 print("Nivel 1, Ejercicio 2")
+it_companies.add("Twitter")
+print(it_companies)
 
-#Nivel 1, Ejercicio 3:
+#Nivel 1, Ejercicio 3: Insertar mas empresas en la lista
 print("Nivel 1, Ejercicio 3")
+it_companies.update("Accenture", "IBM")
+print(it_companies)
 
-#Nivel 1, Ejercicio 4:
+#Nivel 1, Ejercicio 4: Eliminar una empresa de la lista
 print("Nivel 1, Ejercicio 4")
+it_companies.remove("Apple")
+print(it_companies)
 
-#Nivel 1, Ejercicio 5:
+#Nivel 1, Ejercicio 5: Buscar la diferencia entre quitar y desechar
 print("Nivel 1, Ejercicio 5")
+print("Cual es la diferencia entre quitar y desechar?")
+print("")
 
 #Nivel 2, Ejercicio 1:
 print("Nivel 2, Ejercicio 1")
