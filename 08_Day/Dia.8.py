eb2081e9d1b68c4bc5d06a0f4edf6f47938a4151
UP240092 <up240092@alumnos.upa.edu.mx>
2025-03-26 10:16:18 -0600

    dia 8

---
+++ 08_Day/Dia.8.py
@@ -0,0 +1,32 @@
+#Ejercicio 1:
+print("Ejercicio 1")
+
+#Ejercicio 2:
+print("Ejercicio 2")
+
+#Ejercicio 3:
+print("Ejercicio 3")
+
+#Ejercicio 4:
+print("Ejercicio 4")
+
+#Ejercicio 5:
+print("Ejercicio 5")
+
+#Ejercicio 6:
+print("Ejercicio 6")
+
+#Ejercicio 7:
+print("Ejercicio 7")
+
+#Ejercicio 8:
+print("Ejercicio 8")
+
+#Ejercicio 9:
+print("Ejercicio 9")
+
+#Ejercicio 10:
+print("Ejercicio 10")
+
+#Ejercicio 11:
+print("Ejercicio 11")
