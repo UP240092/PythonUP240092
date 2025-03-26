072ae3b533c7d47d6b40d6a5a0bb0436048358b9
UP240092 <up240092@alumnos.upa.edu.mx>
2025-03-26 10:09:16 -0600

    Completo

--- 07_Day/Dia.7.py
+++ 07_Day/Dia.7.py
@@ -10,7 +10,7 @@ print(it_companies)
 
 #Nivel 1, Ejercicio 3: Insertar mas empresas en la lista
 print("Nivel 1, Ejercicio 3")
-it_companies.update("Accenture", "IBM")
+it_companies.update(["Accenture", "IBM"])
 print(it_companies)
 
 #Nivel 1, Ejercicio 4: Eliminar una empresa de la lista
@@ -28,32 +28,62 @@ print("en Python. Se utiliza para eliminar un elemento de un conjunto")
 print("si está presente. Si el elemento no está en el conjunto, no")
 print("hace nada (no lanza una excepción).")
 
-#Nivel 2, Ejercicio 1: 
+#Nivel 2, Ejercicio 1: Unir A y B
 print("Nivel 2, Ejercicio 1")
+A= {19, 22, 24, 20, 25, 26}
+B= {19, 22, 20, 25, 26, 24, 28, 27}
+print(A.union(B))
 
-#Nivel 2, Ejercicio 2:
+#Nivel 2, Ejercicio 2: Buscar una interseccion en B
 print("Nivel 2, Ejercicio 2")
+print(A.intersection(B))
 
-#Nivel 2, Ejercicio 3:
+#Nivel 2, Ejercicio 3: Subconjunto de B
 print("Nivel 2, Ejercicio 3")
+print(A.issubset(B))
 
-#Nivel 2, Ejercicio 4:
+#Nivel 2, Ejercicio 4: Son conjuntos disjuntos A y B?
 print("Nivel 2, Ejercicio 4")
+print(A.isdisjoint(B))
 
-#Nivel 2, Ejercicio 5:
+#Nivel 2, Ejercicio 5: Unir A con B y B con A
 print("Nivel 2, Ejercicio 5")
+print(A.union(B))
+print(B.union(A))
 
-#Nivel 2, Ejercicio 6:
+#Nivel 2, Ejercicio 6: Diferencia simetrica entre A y B
 print("Nivel 2, Ejercicio 6")
+print(A.symmetric_difference(B))
 
-#Nivel 2, Ejercicio 7:
+#Nivel 2, Ejercicio 7: Eliminar los conjuntos por completo
 print("Nivel 2, Ejercicio 7")
+del A
+del B
 
-#Nivel 3, Ejercicio 1:
+#Nivel 3, Ejercicio 1: Edades en un conjunto comparar longitud
 print("Nivel 3, Ejercicio 1")
+edades= [22, 19, 24, 25, 26, 24, 25, 24]
+edadesco= set(edades)
+print(edades)
+print(len(edades))
+print(len(edadesco))
+ed= len(edades) == len(edadesco)
+print("Las longitudes son iguales?: ", ed)
 
-#Nivel 3, Ejercicio 2:
+#Nivel 3, Ejercicio 2: Explicar la diferencia de los diferentes tipos de codigo
 print("Nivel 3, Ejercicio 2")
+print("Explique la diferencia entre los siguientes tipos de datos: cadena, lista, tupla y conjunto")
+print("Las cadenas (str) son secuencias inmutables de caracteres usadas para texto; las listas (list)")
+print("son colecciones ordenadas y mutables que permiten duplicados y cambios en sus elementos; las")
+print("tuplas (tuple) son similares a las listas pero inmutables, también pueden tener duplicados; y")
+print("los conjuntos (set) son colecciones desordenadas y mutables, pero no permiten elementos duplicados,")
+print("siendo útiles para operaciones como la unión e intersección. Cada uno tiene su función dependiendo")
+print("de la necesidad de mutabilidad, orden o exclusividad de elementos.")
 
-#Nivel 3, Ejercicio 3:
+#Nivel 3, Ejercicio 3: Cuantas palabras unicas se han utilizado en la oracion?
 print("Nivel 3, Ejercicio 3")
+fr= "Soy profesora y me encanta inspirar y enseñar a la gente"
+pa= fr.split()
+un= set(pa)
+print(fr)
+print("Las palabras unicas son: ", len(un), un)
