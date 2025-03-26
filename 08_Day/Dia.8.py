68b466afcea1179dcad9629ec5fc563044795ecd
UP240092 <up240092@alumnos.upa.edu.mx>
2025-03-26 10:31:21 -0600

    5

--- 08_Day/Dia.8.py
+++ 08_Day/Dia.8.py
@@ -1,17 +1,41 @@
-#Ejercicio 1:
+#Ejercicio 1: Crea un diccionario vacio llamado perro
 print("Ejercicio 1")
+perro= {}
+print(len(perro))
 
-#Ejercicio 2:
+#Ejercicio 2: Agregar todas las caracteristicas al diccionario
 print("Ejercicio 2")
+perro= {
+    "Nombre": "Beily",
+    "Color": "Cafe",
+    "Raza": "Sharpei",
+    "Patas": 4,
+    "Edad": 4
+}
+print(perro)
 
-#Ejercicio 3:
+#Ejercicio 3: Crea un diccionario para un estudiante
 print("Ejercicio 3")
+estudiante= {
+    "Nombre": "Ana Sofia",
+    "Apellido": "Velasco Lopez",
+    "Sexo": "Femenino",
+    "Edad": 18,
+    "Estado civil": "Soltera",
+    "Habilidades": "Python",
+    "Pais": "Mexico",
+    "Ciudad": "Aguascalientes",
+    "Direccion": "Camino Real #230-A"
+}
+print(estudiante)
 
-#Ejercicio 4:
+#Ejercicio 4: Longitud del diccionario del estudiante
 print("Ejercicio 4")
+print(len(estudiante))
 
-#Ejercicio 5:
+#Ejercicio 5: Valor de habilidades y que tipo de datos son
 print("Ejercicio 5")
+
 
 #Ejercicio 6:
 print("Ejercicio 6")
