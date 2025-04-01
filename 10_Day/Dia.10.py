85366411d9b525571f955f2fd44276458eb56b09
UP240092 <up240092@alumnos.upa.edu.mx>
2025-04-01 08:57:12 -0600

    dia 10

---
+++ 10_Day/Dia.10.py
@@ -0,0 +1,47 @@
+#Nivel 1, Ejercicio 1: 
+print("Nivel 1, Ejercicio 1")
+
+#Nivel 1, Ejercicio 2:
+print("Nivel 1, Ejercicio 2")
+
+#Nivel 1, Ejercicio 3:
+print("Nivel 1, Ejercicio 3")
+
+#Nivel 1, Ejercicio 4:
+print("Nivel 1, Ejercicio 4")
+
+#Nivel 1, Ejercicio 5:
+print("Nivel 1, Ejercicio 5")
+
+#Nivel 1, Ejercicio 6:
+print("Nivel 1, Ejercicio 6")
+
+#Nivel 1, Ejercicio 7:
+print("Nivel 1, Ejercicio 7")
+
+#Nivel 1, Ejercicio 8:
+print("Nivel 1, Ejercicio 8")
+
+#Nivel 2, Ejercicio 1:
+print("Nivel 2, Ejercicio 1")
+
+#Nivel 2, Ejercicio 2:
+print("Nivel 2, Ejercicio 2")
+
+#Nivel 3, Ejercicio 1:
+print("Nivel 3, Ejercicio 1")
+
+#Nivel 3, Ejercicio 2:
+print("Nivel 3, Ejercicio 2")
+
+#Nivel 3, Ejercicio 3.0:
+print("Nivel 3, Ejercicio 3.0")
+
+#Nivel 3, Ejercicio 3.1:
+print("Nivel 3, Ejercicio 3.1")
+
+#Nivel 3, Ejercicio 3.2:
+print("Nivel 3, Ejercicio 3.2")
+
+#Nivel 3, Ejercicio 3.3:
+print("Nivel 3, Ejercicio 3.3")
