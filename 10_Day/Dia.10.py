5cce26f2b133629a83d14401c4d5be880c35b0e7
UP240092 <up240092@alumnos.upa.edu.mx>
2025-04-02 10:43:23 -0600

    Completo

--- 10_Day/Dia.10.py
+++ 10_Day/Dia.10.py
@@ -40,8 +40,8 @@ for i in range(11):
 #Nivel 1, Ejercicio 6: Iterar una lista e imprimir los elementos
 print("Nivel 1, Ejercicio 6")
 le= ['Python', 'Numpy','Pandas','Django', 'Flask']
-for len in le:
-    print(len)
+for lenf in le:
+    print(lenf)
 
 #Nivel 1, Ejercicio 7: Iterar del 0 al 100 y solo imprimir numeros pares
 print("Nivel 1, Ejercicio 7")
@@ -55,30 +55,75 @@ for i in range(1, 100, 2):
 
 #Nivel 2, Ejercicio 1: Usar el bucle for para sumar todos los numeros
 print("Nivel 2, Ejercicio 1")
-su= 0
-n = 0
+suma= 0
 for n in range(101):
-   su= su+n
-   n= n+1
-   print("La suma de todos los numeros es de ", su)
+   suma=suma+n
+print("El total de las suma es de: ", suma)
 
-#Nivel 2, Ejercicio 2:
+#Nivel 2, Ejercicio 2: Iterar del 0 al 100 e imprimir la suma de todos los pares y las probabilidades
 print("Nivel 2, Ejercicio 2")
+supa= 0
+suim= 0
+for n in range(101):
+    if n%2==0:
+      supa=supa+n
+    else:
+      suim= suim+n
+print("La suma de todos los pares es de: ", supa)
+print("La suma de todos los impares es de: ", suim)
 
-#Nivel 3, Ejercicio 1:
+#Nivel 3, Ejercicio 1: Extraer ciertos paises de la lista de paises
 print("Nivel 3, Ejercicio 1")
+from Paises import paises
+print("Paises que contienen la palabra 'Land'")
+for pais in paises:
+   if "land" in pais:
+      print(pais)
 
-#Nivel 3, Ejercicio 2:
+#Nivel 3, Ejercicio 2: Invertir el orden de una lista con un bucle
 print("Nivel 3, Ejercicio 2")
+fru= ['plátano', 'naranja', 'mango', 'limón']
+print("La lista invertida de frutas es: ")
+for fru in reversed(fru):
+   print(fru)
 
-#Nivel 3, Ejercicio 3.0:
-print("Nivel 3, Ejercicio 3.0")
-
-#Nivel 3, Ejercicio 3.1:
+#Nivel 3, Ejercicio 3.1: Cuantos lenguajes hay en la lista de paises
 print("Nivel 3, Ejercicio 3.1")
+import Paises_data as pada
+data= pada.countries
+counle= []
+for pais in data:
+   for language in pais["languages"]:
+      counle.append(language)
+idiomas = set(counle)
+nulen = len(idiomas)
+print("Las cantidades de lenguajes en Paises_data es: ", nulen)
 
-#Nivel 3, Ejercicio 3.2:
+#Nivel 3, Ejercicio 3.2: Encontrar los 10 idiomas mas hablados 
 print("Nivel 3, Ejercicio 3.2")
+paset= set(counle)
+id= {}
+for language in paset:
+   id[language]= 0
+for language in id:
+   for pais in data:
+      if language in pais["languages"]:
+         id[language]= pais["population"]+id[language]
+svlp= sorted(id.values(), reverse= True)
+sklp= sorted(id, key= id.get, reverse= True)
+print("Los 10 idiomas mas hablados en el mundo son: ")
+for i in range(10):
+   print("Idioma: ", sklp[i], "Cantidad de personas hablantes: ", svlp[i])
 
-#Nivel 3, Ejercicio 3.3:
+#Nivel 3, Ejercicio 3.3: Encontrar los 10 paises mas poblados
 print("Nivel 3, Ejercicio 3.3")
+copu= []
+setp= set(copu)
+dicp= {}
+for pais in data:
+   dicp[pais["name"]]= pais["population"]
+sorc= sorted(dicp.items(), key= lambda x: x[1], reverse= True)
+print("Los 10 paises mas poblados son: ")
+for i in range(min(10, len(sorc))):
+   country, population= sorc[i]
+   print(f"{country}: {population}")
