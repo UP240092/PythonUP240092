fc7a3639cf763f5752e38456e4d5ad435d8b7471
UP240092 <up240092@alumnos.upa.edu.mx>
2025-04-03 11:10:33 -0600

    Avance dia 11

---
+++ 11_Day/Dia.11.py
@@ -0,0 +1,89 @@
+#Nivel 1, Ejercicio 1: Poner dos numeros y sumarlos 
+print("Nivel 1, Ejercicio 1")
+def adn():
+    n1= 5
+    n2= 9
+    total= n1+n2
+    return total
+print("El resultado es: ", adn())
+
+#Nivel 1, Ejercicio 2: Calcular el area de un circulo
+print("Nivel 1, Ejercicio 2")
+import math
+def areac():
+    radio= 7
+    area= math.pi*radio**2
+    return area
+print("El area del circulo con un radio de 7 es de: ", areac())
+
+#Nivel 1, Ejercicio 3: Comprobar la lista de argumentos
+print("Nivel 1, Ejercicio 3")
+def addan(*args):
+    if all(isinstance(num, (ont, float)) for num in args):
+        return sum(args) 
+
+#Nivel 1, Ejercicio 4:
+print("Nivel 1, Ejercicio 4")
+
+#Nivel 1, Ejercicio 5:
+print("Nivel 1, Ejercicio 5")
+
+#Nivel 1, Ejercicio 6:
+print("Nivel 1, Ejercicio 6")
+
+#Nivel 1, Ejercicio 7:
+print("Nivel 1, Ejercicio 7")
+
+#Nivel 1, Ejercicio 8:
+print("Nivel 1, Ejercicio 8")
+
+#Nivel 1, Ejercicio 9:
+print("Nivel 1, Ejercicio 9")
+
+#Nivel 1, Ejercicio 10:
+print("Nivel 1, Ejercicio 10")
+
+#Nivel 1, Ejercicio 11:
+print("Nivel 1, Ejercicio 11")
+
+#Nivel 1, Ejercicio 12:
+print("Nivel 1, Ejercicio 12")
+
+#Nivel 1, Ejercicio 13:
+print("Nivel 1, Ejercicio 13")
+
+#Nivel 1, Ejercicio 14:
+print("Nivel 1, Ejercicio 14")
+
+#Nivel 1, Ejercicio 15:
+print("Nivel 1, Ejercicio 15")
+
+#Nivel 2, Ejercicio 1:
+print("Nivel 2, Ejercicio 1")
+
+#Nivel 2, Ejercicio 2:
+print("Nivel 2, Ejercicio 2")
+
+#Nivel 2, Ejercicio 3:
+print("Nivel 2, Ejercicio 3")
+
+#Nivel 2, Ejercicio 4:
+print("Nivel 2, Ejercicio 4")
+
+#Nivel 3, Ejercicio 1:
+print("Nivel 3, Ejercicio 1")
+
+#Nivel 3, Ejercicio 2:
+print("Nivel 3, Ejercicio 2")
+
+#Nivel 3, Ejercicio 3:
+print("Nivel 3, Ejercicio 3")
+
+#Nivel 3, Ejercicio 4:
+print("Nivel 3, Ejercicio 4")
+
+#Nivel 3, Ejercicio 5.1:
+print("Nivel 3, Ejercicio 5.1")
+
+#Nivel 3, Ejercicio 5.2:
+print("Nivel 3, Ejercicio 5.2")
