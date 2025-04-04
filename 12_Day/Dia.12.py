a1349e7f3b7b3f1be854391de4550dd584e87a9c
UP240092 <up240092@alumnos.upa.edu.mx>
2025-04-04 17:30:52 -0600

    avance

---
+++ 12_Day/Dia.12.py
@@ -0,0 +1,28 @@
+import random
+import string
+
+#Nivel 1, Ejercicio 1: Generar un id random
+print("Nivel 1, Ejercicio 1")
+
+
+#Nivel 1, Ejercicio 2:
+print("Nivel 1, Ejercicio 2")
+
+#Nivel 1, Ejercicio 3:
+print("Nivel 1, Ejercicio 3")
+
+#Nivel 2, Ejercicio 1:
+print("Nivel 2, Ejercicio 1")
+
+#Nivel 2, Ejercicio 2:
+print("Nivel 2, Ejercicio 2")
+
+#Nivel 2, Ejercicio 3:
+print("Nivel 2, Ejercicio 3")
+
+#Nivel 3, Ejercicio 1:
+print("Nivel 3, Ejercicio 1")
+
+#Nivel 3, Ejercicio 2:
+print("Nivel 3, Ejercicio 2")
+
