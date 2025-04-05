2d46976f563e3daf623d72d55a0bc8084beab6df
UP240092 <up240092@alumnos.upa.edu.mx>
2025-04-04 18:05:01 -0600

    avance

--- 12_Day/Dia.12.py
+++ 12_Day/Dia.12.py
@@ -33,12 +33,45 @@ print("Colores RGB: ", cg())
 
 #Nivel 2, Ejercicio 1: Colores a matrices
 print("Nivel 2, Ejercicio 1")
+def listOfHexaColors (num):
+    hexaColor = []
+    for j in range(num):
+        randomColor = '#' + ''.join(random.choices('0123456789ABCDEF' , k = 6))
+        hexaColor.append(randomColor)
+    return hexaColor
+print("Números de colores hexadecimales: ")
+print(listOfHexaColors(5))
 
 #Nivel 2, Ejercicio 2: Devolver colores en una matriz
 print("Nivel 2, Ejercicio 2")
+def listOfRgbColors (num):
+    rgb = [] 
+    for c in range(num):
+        red = random.randint(0,255)
+        green = random.randint(0,255)
+        blue = random.randint(0,255)
+        rgb.append(('rgb', red, green, blue))
+    return rgb
+print("Número de colores RGB en una matriz:" , listOfRgbColors(5))
 
 #Nivel 2, Ejercicio 3: Generar numeros de colores
 print("Nivel 2, Ejercicio 3")
+def generateColors (type, num):
+    colors = []
+    if type == 'hexa':
+        for j in range(num):
+            randomColor = '#' + ''.join(random.choices('0123456789ABCDEF', k = 6))
+            colors.append(randomColor)
+        return colors
+    else:
+        for j in range(num):
+            red = random.randint(0,255)
+            green = random.randint(0,255)
+            blue = random.randint(0,255)
+            colors.append(('rgb', red, green, blue))
+        return colors
+print('Colores hexadecimales:', generateColors('hexa', 3))
+print('Colores RGB:', generateColors('rgb', 2))
 
 #Nivel 3, Ejercicio 1: Hacer una lista mesclada
 print("Nivel 3, Ejercicio 1")
---
+++ 15_Day/Dia.15.py
@@ -0,0 +1,1 @@
+#Ejercicio 1: Hacer todos los ejemplos
